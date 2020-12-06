42e54e2f1cf970ce83b004bc632358a9e35a8c6b
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-12-06 11:04:07 +0530

    env

--- env.js
+++ env.js
@@ -1,12 +1,3 @@
 export const dialogflowConfig = {
-  "type": "service_account",
-  "project_id": "bubo-fcoxbb",
-  "private_key_id": "fab8e15dc4320c9917bef14320a0f9366778a53d",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC+hdHLWgt0GqRo\n4zGXkJ0eawUku8lPcb1Tpue6fVTWxAQotBqdl5u5V7zJaPDPjK+9y9R4GWFpHHhP\n648HUesz6Vl4bTJQk2bF26sis8MbQOhK1TDOrNxHK7txhXipH6wahwwnuIrTNXju\nJPtqNJslvas2qRdYi3RGBZhlrX8CPpzxd1+vHAyRX4Qo7PaIGb7b2Y9pJHmEc4p0\nUT+Bv3shtJnB9/QsZIFrUprf4wrF7kTNzaT/jZgSQMUSx5M4HJvnJTE9YEV62Ez+\nu9gsfW83gOeWe1kI5I6fnfDwI7npU7NXLuqxR0fwEBJUSoeYM5rI7vRTZZKU5ZBf\nuwkNUmAjAgMBAAECggEABq3XTHs0IpZahCjO0MLrlyjmTcLdjFDu87NKtWF2a28E\nvE6QDk1F0soOHrt0kbobYRp3sNwm65Hpb9fQtE99YN0B23RsiqGYPUB1r+pAYUC1\nxAXUR7KMqFwSnqiwsbEgDfAFD5FrIHtj6WlXfQIqHDkZevmTjXGaGmhQzuO1q6Tp\nycexk0Tp9JJWjfjKEUXuA6EAg+bQBMOMVTy++DVMXMVhZtjF8BPrO4HW3lZ42dad\nIeVawDr7CkvLp9J3sv1sBUspmIZx6NerXRT9dc1iSwCfT6WYQghx3UtShg1OiGI3\nTme5KogaFjJ9SHjsTXjjZLMS/xXIIKcI6HbEfj/sGQKBgQDlP5q77MYOeBwIJRTm\nq5LW9LpP898YTnCuyQGyyEu+e0kMyUoCTBP/eBs+MrpkjG+E4vSqaSByTzF6ZQ2X\ncBVesR0eQk2gs4cV1JR4Wyi0HgInlseeLT0ydNqoPllQNiDepLjMT8wMgXFgSyrK\nrdQ8pZS822PDWGtkcupne+0faQKBgQDUwVoe8bFkY4076SzL8M6FOqI2Yz1BKuq5\nQzsGPS7P+YQ2UBLAz1aLtUF1ujacuAXM8/xlUuBxVVLurwzFMAfLiHdKAQeQaVIy\nAtZAeX1Hc97gDzNg3iNjU81jZRfYGfX4rI10US7yv9cwuwwm8tHREE1hg566hLfz\nQ+4bofOdqwKBgG/t1CC5BCKAZj9by46saq9lIapzyZNWiRHttlSSEtlX3FzP0sC4\nYHM1Myf1IKiDINfi785cMaleHSxDNpWf9e8bzrm9aaVywhvYhP0WlRMMK8UkiyYh\ntGV0RGYsuvK82uOZzViEBX59nNuFF8mJgbxyo+Jpw5XXD+gla2lHSokxAoGBANAS\nIO8udMy3R8MV8kepxHArvcTvHVMEg/aXt7wMTiH75r+vZAfJPtz3cYOPkCWJG7Mr\n0uXNPc/ZIHhqHlUi+hyl4/QuOZWUS1y5xYvcxRcCTTBX7WmJFpbnuHDCx6UDDrob\nF2RwmTezQuNXqzTUOTr7dJIvxSao8bYp8052/uhtAoGBAMmQRF71nqpR5SKlegqh\nnlyppbqGNz48C/m58+qqQNddyp6dE0OVUV/T3UDyCOJBGda0rW4IikWyHRVTNJ3l\nyiQPuViRMKVT4DIvm9ZikoI6Y+kHHXu/FfGqNMkjOcGIKfhYBbJlObZHSRVNbDw2\nWTs89cyqv0zTSrv4MzTDTjjR\n-----END PRIVATE KEY-----\n",
-  "client_email": "dialogflow-ldkeoc@bubo-fcoxbb.iam.gserviceaccount.com",
-  "client_id": "109594013897827992024",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-ldkeoc%40bubo-fcoxbb.iam.gserviceaccount.com"
+  // your credentials here
 }
